748b3f9f6c749ccfcff7bc4bb2f6cf4109804251
Yasu <yasu@fuchslativ.de>
2023-11-10 11:40:15 +0100

    :tada: Commit testing plugin

---
+++ src/Plugin.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Hexafuchs\PsalmLaminas;
+
+use SimpleXMLElement;
+use Psalm\Plugin\PluginEntryPointInterface;
+use Psalm\Plugin\RegistrationInterface;
+
+class Plugin implements PluginEntryPointInterface
+{
+    /**
+     * @param \Psalm\Plugin\RegistrationInterface $registration
+     * @param \SimpleXMLElement|null              $config
+     *
+     * @return void
+     */
+    public function __invoke(RegistrationInterface $registration, ?SimpleXMLElement $config = null): void
+    {
+        // This is plugin entry point. You can initialize things you need here,
+        // and hook them into psalm using RegistrationInterface
+        //
+        // Here's some examples:
+        // 1. Add a stub file
+        // ```php
+        // $psalm->addStubFile(__DIR__ . '/stubs/YourStub.php');
+        // ```
+        foreach ($this->getStubFiles() as $file) {
+            $registration->addStubFile($file);
+        }
+
+        // Psalm allows arbitrary content to be stored under you plugin entry in
+        // its config file, psalm.xml, so your plugin users can put some configuration
+        // values there. They will be provided to your plugin entry point in $config
+        // parameter, as a SimpleXmlElement object. If there's no configuration present,
+        // null will be passed instead.
+    }
+
+    /** @return list<string> */
+    private function getStubFiles(): array
+    {
+        return glob(__DIR__ . '/../stubs/*.phpstub') ?: [];
+    }
+}
